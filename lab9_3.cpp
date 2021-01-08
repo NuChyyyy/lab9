fd9092b16edebf7ed227a997d6866748662c66df
NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>
2021-01-08 16:56:42 +0700

    lab9-3*edit
    Signed-off-by: NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    double sum = 0,count = 0,ave,sumsum = 0;
+    ifstream score;
+    score.open("score.txt");
+    string text;
+    while(getline(score,text))
+    {
+        sum += stod(text); 
+        sumsum += pow((stod(text)),2);
+        count++;
+    }
+    ave = sum/count;
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << ave << "\n";
+    cout << "Standard deviation = " << sqrt(1/count*(sumsum)-pow(ave,2));
+}
